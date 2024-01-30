afbdbb764cdcc39fff89303950c3cc8ba258350f
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-30 15:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function stringChop(str, size) {
   // your code here
+	let len=size;
+	 if(len<=0){
+        return [];
+    }
+
+    let result=[];
+
+    for(let i=0;i<str.length;i=i+len){
+        let sub=str.substring(i,i+len);
+        result.push(sub);
+        // console.log(sub)
+    }
+
+    return result;
+
+
+
+
 }
 
 // Do not change the code below
